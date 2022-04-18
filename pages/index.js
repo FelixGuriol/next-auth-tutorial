63df3dec287951ac6d43bf60c2c4ef579313a3ca
Felix Gutierrez <lixfe2000g@gmail.com>
2022-04-18 15:58:19 -0500

    Auth only with getServerSideProps and login

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,27 @@
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { getSession } from "next-auth/react";
 
-function HomPage() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
-
-  if (status === "unauthenticated") {
-    router.push("/login");
-  }
-
+function HomPage({ session }) {
   return (
     <div>
-      {session ? (
-        <div>
-          <h1>{session.user.name}</h1>
-          <p>{session.user.email}</p>
-          <img src={session.user.image} />
-        </div>
-      ) : (
-        <p>Skeleton zs</p>
-      )}
+      <h1>{session.user.name}</h1>
+      <p>{session.user.email}</p>
+      <img src={session.user.image} />
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+  if (!session)
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  return {
+    props: { session },
+  };
+};
 
 export default HomPage;
